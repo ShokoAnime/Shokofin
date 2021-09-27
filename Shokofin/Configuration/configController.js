131cf6e9a7778204aef0ae23ca32be5998fb4c08
Mikal Stordal <mikalstordal@gmail.com>
2021-09-27 22:35:42 +0200

    Fix the plugin configuration page. Fix the "Pressing Enter when logged in logs you out" issue, and the unable to connect to a different host if Shoko Server is not running locally.

--- Shokofin/Configuration/configController.js
+++ Shokofin/Configuration/configController.js
@@ -2,39 +2,47 @@ const PluginConfig = {
     pluginId: "5216ccbf-d24a-4eb3-8a7e-7da4230b7052"
 };
 
-function loadUserConfig(page, userId) {
+const Messages = {
+    ConnectToShoko: "Please establish a connection to a running instance of Shoko Server before you continue.",
+    InvalidCredentials: "An error occured while trying to authenticating the user using the provided credentials.",
+    UnableToRender: "There was an error loading the page, please refresh once to see if that will fix it.",
+};
+
+async function loadUserConfig(page, userId, config) {
     if (!userId) {
         page.querySelector("#UserSettingsContainer").setAttribute("hidden", "");
         page.querySelector("#UserUsername").removeAttribute("required");
         Dashboard.hideLoadingMsg();
         return;
     }
-
-    page.querySelector("#UserSettingsContainer").removeAttribute("hidden");
+    
     Dashboard.showLoadingMsg();
 
-    return ApiClient.getPluginConfiguration(PluginConfig.pluginId).then(function (config) {
-        const userConfig = config.UserList.find((c) => userId === c.UserId) || { UserId: userId };
+    // Get the configuration to use.
+    if (!config) config = await ApiClient.getPluginConfiguration(PluginConfig.pluginId)
+    const userConfig = config.UserList.find((c) => userId === c.UserId) || { UserId: userId };
 
-        page.querySelector("#UserEnableSynchronization").checked = userConfig.EnableSynchronization || false;
-        page.querySelector("#UserUsername").value = userConfig.Username || "";
-        page.querySelector("#UserPassword").value = "";
+    // Configure the elements within the user container
+    page.querySelector("#UserEnableSynchronization").checked = userConfig.EnableSynchronization || false;
+    page.querySelector("#UserUsername").value = userConfig.Username || "";
+    page.querySelector("#UserPassword").value = "";
+    if (userConfig.Token) {
+        page.querySelector("#UserDeleteContainer").removeAttribute("hidden");
+        page.querySelector("#UserUsername").setAttribute("disabled", "");
+        page.querySelector("#UserPasswordContainer").setAttribute("hidden", "");
+        page.querySelector("#UserUsername").removeAttribute("required");
+    }
+    else {
+        page.querySelector("#UserDeleteContainer").setAttribute("hidden", "");
+        page.querySelector("#UserUsername").removeAttribute("disabled");
+        page.querySelector("#UserPasswordContainer").removeAttribute("hidden");
+        page.querySelector("#UserUsername").setAttribute("required", "");
+    }
 
-        if (userConfig.Token) {
-            page.querySelector("#UserDeleteContainer").removeAttribute("hidden");
-            page.querySelector("#UserUsername").setAttribute("disabled", "");
-            page.querySelector("#UserPasswordContainer").setAttribute("hidden", "");
-            page.querySelector("#UserUsername").removeAttribute("required");
-        }
-        else {
-            page.querySelector("#UserDeleteContainer").setAttribute("hidden", "");
-            page.querySelector("#UserUsername").removeAttribute("disabled");
-            page.querySelector("#UserPasswordContainer").removeAttribute("hidden");
-            page.querySelector("#UserUsername").setAttribute("required", "");
-        }
+    // Show the user settings now if it was previously hidden.
+    page.querySelector("#UserSettingsContainer").removeAttribute("hidden");
 
-        Dashboard.hideLoadingMsg();
-    });
+    Dashboard.hideLoadingMsg();
 }
 
 function getApiKey(username, password) {
@@ -53,44 +61,116 @@ function getApiKey(username, password) {
     });
 }
 
-async function resetConnectionSettings(form) {
+async function defaultSubmit(form) {
     const config = await ApiClient.getPluginConfiguration(PluginConfig.pluginId);
-    let host = form.querySelector("#Host").value;
-    if (host.endsWith("/")) {
-        host = host.slice(0, -1);
-        form.querySelector("#Host").value = host;
+
+    if (config.ApiKey !== "") {
+        let publicHost = form.querySelector("#PublicHost").value;
+        if (publicHost.endsWith("/")) {
+            publicHost = publicHost.slice(0, -1);
+            form.querySelector("#PublicHost").value = publicHost;
+        }
+
+        // Metadata settings
+        config.TitleMainType = form.querySelector("#TitleMainType").value;
+        config.TitleAlternateType = form.querySelector("#TitleAlternateType").value;
+        config.DescriptionSource = form.querySelector("#DescriptionSource").value;
+        config.SynopsisCleanLinks = form.querySelector("#CleanupAniDBDescriptions").checked;
+        config.SynopsisCleanMultiEmptyLines = form.querySelector("#CleanupAniDBDescriptions").checked;
+        config.SynopsisCleanMiscLines = form.querySelector("#MinimalAniDBDescriptions").checked;
+        config.SynopsisRemoveSummary = form.querySelector("#MinimalAniDBDescriptions").checked;
+    
+        // Library settings
+        config.SeriesGrouping = form.querySelector("#SeriesGrouping").value;
+        config.BoxSetGrouping = form.querySelector("#BoxSetGrouping").value;
+        config.FilterOnLibraryTypes = form.querySelector("#FilterOnLibraryTypes").checked;
+        config.SpecialsPlacement = form.querySelector("#SpecialsPlacement").value;
+        config.MarkSpecialsWhenGrouped = form.querySelector("#MarkSpecialsWhenGrouped").checked;
+    
+        // Tag settings
+        config.HideArtStyleTags = form.querySelector("#HideArtStyleTags").checked;
+        config.HideSourceTags = form.querySelector("#HideSourceTags").checked;
+        config.HideMiscTags = form.querySelector("#HideMiscTags").checked;
+        config.HidePlotTags = form.querySelector("#HidePlotTags").checked;
+        config.HideAniDbTags = form.querySelector("#HideAniDbTags").checked;
+    
+        // Advanced settings
+        config.PublicHost = publicHost;
+        config.PreferAniDbPoster = form.querySelector("#PreferAniDbPoster").checked;
+        config.AddAniDBId = form.querySelector("#AddAniDBId").checked;
+
+        // User settings
+        const userId = form.querySelector("#UserSelector").value;
+        if (userId) {
+            let userConfig = config.UserList.find((c) => userId === c.UserId);
+            if (!userConfig) {
+                userConfig = { UserId: userId };
+                config.UserList.push(userConfig);
+            }
+            
+            // The user settings goes below here;
+            userConfig.EnableSynchronization = form.querySelector("#UserEnableSynchronization").checked;
+            
+            // Only try to save a new token if a token is not already present.
+            const username = form.querySelector("#UserUsername").value;
+            const password = form.querySelector("#UserPassword").value;
+            if (!userConfig.Token) try {
+                const response = await getApiKey(username, password);
+                userConfig.Username = username;
+                userConfig.Token = response.apikey;
+            }
+            catch (err) {
+                Dashboard.alert(Messages.InvalidCredentials);
+                console.error(err, Messages.InvalidCredentials);
+                userConfig.Username = "";
+                userConfig.Token = "";
+            }
+        }
+
+        let result = await ApiClient.updatePluginConfiguration(PluginConfig.pluginId, config);
+        Dashboard.processPluginConfigurationUpdateResult(result);
     }
+    else {
+        // Connection settings
+        let host = form.querySelector("#Host").value;
+        if (host.endsWith("/")) {
+            host = host.slice(0, -1);
+            form.querySelector("#Host").value = host;
+        }
 
-    const username = form.querySelector("#Username").value = config.Username;
-    form.querySelector("#Password").value = "";
+        // Update the host if needed.
+        if (config.Host !== host) {
+            config.Host = host;
+            let result = await ApiClient.updatePluginConfiguration(PluginConfig.pluginId, config);
+            Dashboard.processPluginConfigurationUpdateResult(result);
+        }
 
-    // Connection settings
-    config.Host = host;
-    config.Username = username;
-    config.ApiKey = "";
-
-    const result = await ApiClient.updatePluginConfiguration(PluginConfig.pluginId, config);
-    Dashboard.processPluginConfigurationUpdateResult(result);
+        const username = form.querySelector("#Username").value;
+        const password = form.querySelector("#Password").value;
+        try {
+            const response = await getApiKey(username, password);
+            config.Username = username;
+            config.ApiKey = response.apikey;
+        
+            let result = await ApiClient.updatePluginConfiguration(PluginConfig.pluginId, config);
+            Dashboard.processPluginConfigurationUpdateResult(result);
+        }
+        catch (err) {
+            Dashboard.alert(Messages.InvalidCredentials);
+            console.error(err, Messages.InvalidCredentials);
+        }
+    }
 
     return config;
 }
-async function setConnectionSettings(form) {
+
+async function resetConnectionSettings(form) {
     const config = await ApiClient.getPluginConfiguration(PluginConfig.pluginId);
-    let host = form.querySelector("#Host").value;
-    if (host.endsWith("/")) {
-        host = host.slice(0, -1);
-        form.querySelector("#Host").value = host;
-    }
-
-    const username = form.querySelector("#Username").value;
-    const password = form.querySelector("#Password").value;
-
-    const response = await getApiKey(username, password);
-
+    form.querySelector("#Username").value = config.Username;
+    form.querySelector("#Password").value = "";
+    
     // Connection settings
-    config.Host = host;
-    config.Username = username;
-    config.ApiKey = response.apikey;
+    config.ApiKey = "";
 
     const result = await ApiClient.updatePluginConfiguration(PluginConfig.pluginId, config);
     Dashboard.processPluginConfigurationUpdateResult(result);
@@ -178,7 +258,8 @@ async function syncUserSettings(form) {
         userConfig.Token = response.apikey;
     }
     catch (err) {
-        Dashboard.alert(`An error occured while trying to authenticating the user using the provided credentials; ${err.message}`);
+        Dashboard.alert(Messages.InvalidCredentials);
+        console.error(err, Messages.InvalidCredentials);
         userConfig.Username = "";
         userConfig.Token = "";
     }
@@ -192,6 +273,7 @@ async function syncUserSettings(form) {
 export default function (page) {
     const form = page.querySelector("#ShokoConfigForm");
     const userSelector = form.querySelector("#UserSelector");
+    // Refresh the view after we changed the settings, so the view reflect the new settings.
     const refershSettings = (config) => {
         if (config.ApiKey) {
             form.querySelector("#Host").setAttribute("disabled", "");
@@ -221,11 +303,10 @@ export default function (page) {
             form.querySelector("#AdvancedSection").setAttribute("hidden", "");
         }
 
-        userSelector.dispatchEvent(new Event("change", {
-            bubbles: true,
-            cancelable: false
-        }));
+        const userId = form.querySelector("#UserSelector").value;
+        loadUserConfig(page, userId, config);
     };
+
     const onError = (err) => {
         console.error(err);
         Dashboard.alert(`An error occurred; ${err.message}`);
@@ -277,13 +358,14 @@ export default function (page) {
             form.querySelector("#AddAniDBId").checked = config.AddAniDBId;
 
             if (!config.ApiKey) {
-                Dashboard.alert("Please establish a connection to a running instance of Shoko Server before you continue.");
+                Dashboard.alert(Messages.ConnectToShoko);
             }
 
             refershSettings(config);
         }
-        catch {
-            Dashboard.alert("There was an error loading the page, please refresh once to see if that will fix it.");
+        catch (err) {
+            Dashboard.alert(Messages.UnableToRender);
+            console.error(err, Messages.UnableToRender)
             Dashboard.hideLoadingMsg();
         }
     });
@@ -293,6 +375,9 @@ export default function (page) {
         if (!event.submitter) return;
         switch (event.submitter.name) {
             default:
+            case "all-settings":
+                Dashboard.showLoadingMsg();
+                defaultSubmit(form).then(refershSettings).catch(onError);
                 break;
             case "settings":
                 Dashboard.showLoadingMsg();
@@ -301,10 +386,6 @@ export default function (page) {
             case "reset-connection":
                 Dashboard.showLoadingMsg();
                 resetConnectionSettings(form).then(refershSettings).catch(onError);
-                break;
-            case "set-connection":
-                Dashboard.showLoadingMsg();
-                setConnectionSettings(form).then(refershSettings).catch(onError);
                 break;
             case "unlink-user":
                 unlinkUser(form).then(refershSettings).catch(onError);
