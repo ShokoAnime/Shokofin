b5279278cc5c4179425d5c321949c917eb37bbdd
Mikal S <revam@users.noreply.github.com>
2022-11-29 17:48:19 +0100

    ignore directories and all sub-entries

--- Shokofin/LibraryScanner.cs
+++ Shokofin/LibraryScanner.cs
@@ -50,7 +50,7 @@ namespace Shokofin
 
                 if (fileInfo.IsDirectory &&  Plugin.Instance.IgnoredFolders.Contains(Path.GetFileName(fileInfo.FullName).ToLowerInvariant())) {
                     Logger.LogDebug("Skipped excluded folder at path {Path}", fileInfo.FullName);
-                    return false;
+                    return true;
                 }
 
                 if (!fileInfo.IsDirectory && Plugin.Instance.IgnoredFileExtensions.Contains(fileInfo.Extension.ToLowerInvariant())) {
