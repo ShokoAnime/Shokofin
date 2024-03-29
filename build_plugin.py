02b1a6f5a18f0215ab2ed29fc4dd62fbc9e67f03
Mikal Stordal <mikalstordal@gmail.com>
2024-03-29 07:38:26 +0100

    misc: add changelog to manifest releases
    [skip ci]

--- build_plugin.py
+++ build_plugin.py
@@ -1,5 +1,6 @@
 ﻿import os
-import sys
+import json
+import yaml
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -20,8 +21,32 @@ else:
 
 jellyfin_repo_url="https://github.com/ShokoAnime/Shokofin/releases/download"
 
+# Add changelog to the build yaml before we generate the release.
+build_file = './build.yaml'
+
+with open(build_file, 'r') as file:
+    data = yaml.safe_load(file)
+
+if "changelog" in data:
+    data["changelog"] = os.environ["CHANGELOG"].strip()
+
+with open(build_file, 'w') as file:
+    yaml.dump(data, file, sort_keys=False)
+
 zipfile=os.popen('jprm --verbosity=debug plugin build "." --output="%s" --version="%s" --dotnet-framework="net6.0"' % (artifact_dir, version)).read().strip()
 
 os.system('jprm repo add --url=%s %s %s' % (jellyfin_repo_url, jellyfin_repo_file, zipfile))
+
+# Compact the unstable manifest after building, so it only contains the last 5 versions.
+if prerelease:
+  with open(jellyfin_repo_file, 'r') as file:
+      data = json.load(file)
+
+  for item in data:
+      if 'versions' in item and len(item['versions']) > 5:
+          item['versions'] = item['versions'][:5]
+
+  with open(jellyfin_repo_file, 'w') as file:
+      json.dump(data, file, indent=4)
 
 print(version)
