c1b290e5a2cef0d563a56b513f6bf2c25f907010
Mikal Stordal <mikalstordal@gmail.com>
2024-05-22 04:40:27 +0200

    feat: add trailer & video providers
    - Added a trailer and video provider responsible for fetching metadata for theme videos, special featurettes, and trailers __for entities in the VFS__.

---
+++ Shokofin/Providers/VideoProvider.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Providers;
+using Microsoft.Extensions.Logging;
+using Shokofin.API;
+using Shokofin.Utils;
+
+namespace Shokofin.Providers;
+
+public class VideoProvider: IRemoteMetadataProvider<Video, ItemLookupInfo>, IHasOrder
+{
+    public string Name => Plugin.MetadataProviderName;
+
+    // Always run first, so we can react to the VFS entries.
+    public int Order => -1;
+
+    private readonly IHttpClientFactory HttpClientFactory;
+
+    private readonly ILogger<VideoProvider> Logger;
+
+    private readonly ShokoAPIManager ApiManager;
+
+    public VideoProvider(IHttpClientFactory httpClientFactory, ILogger<VideoProvider> logger, ShokoAPIManager apiManager)
+    {
+        HttpClientFactory = httpClientFactory;
+        Logger = logger;
+        ApiManager = apiManager;
+    }
+
+    public async Task<MetadataResult<Video>> GetMetadata(ItemLookupInfo info, CancellationToken cancellationToken)
+    {
+        try {
+            var result = new MetadataResult<Video>();
+            var config = Plugin.Instance.Configuration;
+            if (string.IsNullOrEmpty(info.Path) || !info.Path.StartsWith(Plugin.Instance.VirtualRoot + Path.DirectorySeparatorChar)) {
+                return result;
+            }
+
+            var (fileInfo, seasonInfo, showInfo) = await ApiManager.GetFileInfoByPath(info.Path);
+            var episodeInfo = fileInfo?.EpisodeList.FirstOrDefault().Episode;
+            if (fileInfo == null || episodeInfo == null || seasonInfo == null || showInfo == null) {
+                Logger.LogWarning("Unable to find episode info for path {Path}", info.Path);
+                return result;
+            }
+
+            var (displayTitle, alternateTitle) = Text.GetEpisodeTitles(episodeInfo, seasonInfo, info.MetadataLanguage);
+            var description = Text.GetDescription(episodeInfo);
+            result.Item = new()
+            {
+                Name = displayTitle,
+                OriginalTitle = alternateTitle,
+                PremiereDate = episodeInfo.AniDB.AirDate,
+                ProductionYear = episodeInfo.AniDB.AirDate?.Year ?? seasonInfo.AniDB.AirDate?.Year,
+                Overview = description,
+                CommunityRating = episodeInfo.AniDB.Rating.Value > 0 ? episodeInfo.AniDB.Rating.ToFloat(10) : 0,
+            };
+            Logger.LogInformation("Found video {EpisodeName} (File={FileId},Episode={EpisodeId},Series={SeriesId},Group={GroupId})", result.Item.Name, fileInfo.Id, episodeInfo.Id, seasonInfo.Id, showInfo?.GroupId);
+
+            result.HasMetadata = true;
+
+            return result;
+        }
+        catch (Exception ex) {
+            Logger.LogError(ex, "Threw unexpectedly; {Message}", ex.Message);
+            return new MetadataResult<Video>();
+        }
+    }
+
+    public Task<IEnumerable<RemoteSearchResult>> GetSearchResults(ItemLookupInfo searchInfo, CancellationToken cancellationToken)
+        => Task.FromResult<IEnumerable<RemoteSearchResult>>(new List<RemoteSearchResult>());
+
+    public Task<HttpResponseMessage> GetImageResponse(string url, CancellationToken cancellationToken)
+        => HttpClientFactory.CreateClient().GetAsync(url, cancellationToken);
+}
