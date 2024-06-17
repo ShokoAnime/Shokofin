ShokoAnime/Shokofin

commit 686785c4b004c29b02b267f8d35a9242f84f9b5b

cleanup: simplify attribute usage

Shokofin/Utils/TagFilter.cs
@@ -434,26 +434,28 @@ public static class TagFilter
             return new() { GetSourceMaterial(tags) };
 
         var tagSet = new HashSet<string>();
-        var includeTags = new List<KeyValuePair<string, ResolvedTag>>();
         var exceptTags = new List<ResolvedTag>();
-        var field = source.GetType().GetField(source.ToString());
-        if (field?.GetCustomAttributes(typeof(TagSourceIncludeAttribute), false) is TagSourceIncludeAttribute[] includeAttributes && includeAttributes.Length is not 0)
-            foreach (var tagName in includeAttributes.First().Values)
-                if (tags.TryGetValue(tagName, out var tag))
-                    includeTags.AddRange(tag.RecursiveNamespacedChildren);
-        if (field?.GetCustomAttributes(typeof(TagSourceIncludeOnlyAttribute), false) is TagSourceIncludeOnlyAttribute[] includeOnlyAttributes && includeOnlyAttributes.Length is not 0)
-            foreach (var tagName in includeOnlyAttributes.First().Values)
-                if (tags.TryGetValue(tagName, out var tag))
-                    includeTags.Add(KeyValuePair.Create($"/{tag.Name}", tag));
-        if (field?.GetCustomAttributes(typeof(TagSourceExcludeAttribute), false) is TagSourceExcludeAttribute[] excludeAttributes && excludeAttributes.Length is not 0)
-            foreach (var tagName in excludeAttributes.First().Values)
-                if (tags.TryGetValue(tagName, out var tag))
-                    exceptTags.AddRange(tag.RecursiveNamespacedChildren.Values.Append(tag));
+        var includeTags = new List<KeyValuePair<string, ResolvedTag>>();
+        var field = source.GetType().GetField(source.ToString())!;
+        var includeAttributes = field.GetCustomAttributes<TagSourceIncludeAttribute>();
+        foreach (var tagName in includeAttributes.First().Values)
+            if (tags.TryGetValue(tagName, out var tag))
+                includeTags.AddRange(tag.RecursiveNamespacedChildren);
 
-        if (field?.GetCustomAttributes(typeof(TagSourceExcludeOnlyAttribute), false) is TagSourceExcludeOnlyAttribute[] excludeOnlyAttributes && excludeOnlyAttributes.Length is not 0)
-            foreach (var tagName in excludeOnlyAttributes.First().Values)
-                if (tags.TryGetValue(tagName, out var tag))
-                    exceptTags.Add(tag);
+        var includeOnlyAttributes = field.GetCustomAttributes<TagSourceIncludeOnlyAttribute>();
+        foreach (var tagName in includeOnlyAttributes.First().Values)
+            if (tags.TryGetValue(tagName, out var tag))
+                includeTags.Add(KeyValuePair.Create($"/{tag.Name}", tag));
+
+        var excludeAttributes = field.GetCustomAttributes<TagSourceExcludeAttribute>();
+        foreach (var tagName in excludeAttributes.First().Values)
+            if (tags.TryGetValue(tagName, out var tag))
+                exceptTags.AddRange(tag.RecursiveNamespacedChildren.Values.Append(tag));
+
+        var excludeOnlyAttributes = field.GetCustomAttributes<TagSourceExcludeOnlyAttribute>();
+        foreach (var tagName in excludeOnlyAttributes.First().Values)
+            if (tags.TryGetValue(tagName, out var tag))
+                exceptTags.Add(tag);
 
         includeTags = includeTags
             .DistinctBy(pair => $"{pair.Value.Source}:{pair.Value.Id}")

Shokofin/Utils/ContentRating.cs
@@ -350,20 +350,23 @@ public static class ContentRating
         return true;
     }
 
+    internal static T[] GetCustomAttributes<T>(this System.Reflection.FieldInfo? fieldInfo, bool inherit = false)
+        => fieldInfo?.GetCustomAttributes(typeof(T), inherit) is T[] attributes ? attributes : Array.Empty<T>();
+
     private static string? ConvertRatingToText(TvRating value, IEnumerable<TvContentIndicator>? contentIndicators)
     {
-        var field = value.GetType().GetField(value.ToString());
-        if (field?.GetCustomAttributes(typeof(DescriptionAttribute), false) is DescriptionAttribute[] attributes && attributes.Length != 0) {
-            var contentRating = attributes.First().Description;
-            var allowedIndicators = (
-                (field.GetCustomAttributes(typeof(TvContentIndicatorsAttribute), false) as TvContentIndicatorsAttribute[] ?? Array.Empty<TvContentIndicatorsAttribute>()).FirstOrDefault()?.Values ?? Array.Empty<TvContentIndicator>()
-            )
-                .Intersect(contentIndicators ?? Array.Empty<TvContentIndicator>())
-                .ToList();
-            if (allowedIndicators.Count is > 0)
-                contentRating += $"-{allowedIndicators.Select(cI => cI.ToString()).Join("")}";
-            return contentRating;
-        }
-        return null;
+        var field = value.GetType().GetField(value.ToString())!;
+        var attributes = field.GetCustomAttributes<DescriptionAttribute>();
+        if (attributes.Length is 0)
+            return null;
+
+        var contentRating = attributes.First().Description;
+        var allowedIndicators = (field.GetCustomAttributes<TvContentIndicatorsAttribute>().FirstOrDefault()?.Values ?? Array.Empty<TvContentIndicator>())
+            .Intersect(contentIndicators ?? Array.Empty<TvContentIndicator>())
+            .ToList();
+        if (allowedIndicators.Count is > 0)
+            contentRating += $"-{allowedIndicators.Select(cI => cI.ToString()).Join("")}";
+
+        return contentRating;
     }
 }

commit 791c568f276ecdf8ec4fbc80c8c02fa556876b96

fix: fix regression
Fixes a regression introduced in 686785c4b004c29b02b267f8d35a9242f84f9b5b by accidentally removing _a few_ (4) critical checks before using some values

Shokofin/Utils/TagFilter.cs
@@ -438,24 +438,28 @@ public static class TagFilter
         var includeTags = new List<KeyValuePair<string, ResolvedTag>>();
         var field = source.GetType().GetField(source.ToString())!;
         var includeAttributes = field.GetCustomAttributes<TagSourceIncludeAttribute>();
-        foreach (var tagName in includeAttributes.First().Values)
-            if (tags.TryGetValue(tagName, out var tag))
-                includeTags.AddRange(tag.RecursiveNamespacedChildren);
+        if (includeAttributes.Length is 1)
+            foreach (var tagName in includeAttributes.First().Values)
+                if (tags.TryGetValue(tagName, out var tag))
+                    includeTags.AddRange(tag.RecursiveNamespacedChildren);
 
         var includeOnlyAttributes = field.GetCustomAttributes<TagSourceIncludeOnlyAttribute>();
-        foreach (var tagName in includeOnlyAttributes.First().Values)
-            if (tags.TryGetValue(tagName, out var tag))
-                includeTags.Add(KeyValuePair.Create($"/{tag.Name}", tag));
+        if (includeOnlyAttributes.Length is 1)
+            foreach (var tagName in includeOnlyAttributes.First().Values)
+                if (tags.TryGetValue(tagName, out var tag))
+                    includeTags.Add(KeyValuePair.Create($"/{tag.Name}", tag));
 
         var excludeAttributes = field.GetCustomAttributes<TagSourceExcludeAttribute>();
-        foreach (var tagName in excludeAttributes.First().Values)
-            if (tags.TryGetValue(tagName, out var tag))
-                exceptTags.AddRange(tag.RecursiveNamespacedChildren.Values.Append(tag));
+        if (excludeAttributes.Length is 1)
+            foreach (var tagName in excludeAttributes.First().Values)
+                if (tags.TryGetValue(tagName, out var tag))
+                    exceptTags.AddRange(tag.RecursiveNamespacedChildren.Values.Append(tag));
 
         var excludeOnlyAttributes = field.GetCustomAttributes<TagSourceExcludeOnlyAttribute>();
-        foreach (var tagName in excludeOnlyAttributes.First().Values)
-            if (tags.TryGetValue(tagName, out var tag))
-                exceptTags.Add(tag);
+        if (excludeOnlyAttributes.Length is 1)
+            foreach (var tagName in excludeOnlyAttributes.First().Values)
+                if (tags.TryGetValue(tagName, out var tag))
+                    exceptTags.Add(tag);
 
         includeTags = includeTags
             .DistinctBy(pair => $"{pair.Value.Source}:{pair.Value.Id}")