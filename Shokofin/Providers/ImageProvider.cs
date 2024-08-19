18849ae6af5d1adf92c3de1daebcfc5f3e8a0ac4
Mikal Stordal <mikalstordal@gmail.com>
2024-08-20 00:54:54 +0200

    fix: remove image language code again
    - Removed the language code for images, since it was interfering with the _preferred_ image selection done by shoko. It seems Jellyfin has it's own image selection logic, and it doesn't just pick the first image available… it picks the first image available in the metadata language of the library… which defaults to `"en"` if not set… and that might not be preferred for our use case. We'll see if we can partially add it back later for some image types, e.g. logos, etc., but for now it's gone.

--- Shokofin/Providers/ImageProvider.cs
+++ Shokofin/Providers/ImageProvider.cs
@@ -163,7 +163,6 @@ public class ImageProvider : IRemoteImageProvider, IHasOrder
             return;
         list.Add(new RemoteImageInfo {
             ProviderName = Plugin.MetadataProviderName,
-            Language = image.LanguageCode,
             Type = imageType,
             Width = image.Width,
             Height = image.Height,
