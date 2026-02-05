29d1d382f0ea06aa17468dfd1091f78f17abcce0
Mikal Stordal <mikalstordal@gmail.com>
2026-02-05 05:46:24 +0100

    refactor: `ThemeSong` → `Credits` in APIv3

--- Shokofin/API/Models/EpisodeType.cs
+++ Shokofin/API/Models/EpisodeType.cs
@@ -30,9 +30,14 @@ public enum EpisodeType {
     Trailer = 4,
 
     /// <summary>
+    /// An opening song, ending song, or other type of credits.
+    /// </summary>
+    Credits = 5,
+
+    /// <summary>
     /// Either an opening-song, or an ending-song.
     /// </summary>
-    ThemeSong = 5,
+    ThemeSong = Credits,
 
     /// <summary>
     /// Intro, and/or opening-song.
